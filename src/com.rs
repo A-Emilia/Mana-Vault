89e27f2f39e395b24449c647c3fab93f40cfd866
A-Emilia <CHKW@live.dk>
2025-04-17 00:01:04 +0200

    com update

--- src/com.rs
+++ src/com.rs
@@ -1,5 +1,26 @@
+use crate::model::Card;
 
-pub struct Request;
+
+pub struct Request {
+    pub request_type: RequestType,
+}
+
+pub enum RequestType {
+    // Is this just for cards? How do I handle putting a card a specific place????
+    GetById(usize),
+    GetByName(String),
+    GetBySet(String),
+}
+
+impl Request {
+    // I feel like I am just going down a rabbithole of abstraction.
+    // Java did a number on me.
+    pub fn get_by_id(id: usize) -> Self {
+        Self {
+        request_type: RequestType::GetById(id)
+        }
+    }
+}
 pub struct Response;
 pub struct RequestBuilder;
 pub struct ResponseBuilder;
